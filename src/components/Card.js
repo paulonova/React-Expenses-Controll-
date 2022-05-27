b9d5e7d411b0c28b1242119ea25f56df7ebde9fa
Paulo Nova <paulovilanova@MacBook-Pro-som-tillhor-Paulo.local>
2022-05-27 12:31:02 +0200

    Card and some changings

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import "../css/card.css";
+
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
 import "../css/expenseitem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-function ExpenseItem(props) {
+function expenses(props) {
   return (
-    <div className='expense-item'>
-      <div>
-        <div>{props.date.toLocaleString("en-GB", { month: "long" })}</div>
-        <div>Year</div>
-        <div>Day</div>
-      </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <p className='expense-item__price'>${props.amount}</p>
       </div>
-    </div>
+    </Card>
   );
 }
 
-export default ExpenseItem;
+export default expenses;
